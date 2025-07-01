d071589ad9e888943a1fa02a2d25cc8bf1163865
Abdulrahman Elnory <135131957+abdulrahmanelnory1@users.noreply.github.com>
2025-07-01 04:49:36 +0300

    Update Repository.java

--- gitlet/Repository.java
+++ gitlet/Repository.java
@@ -31,7 +31,7 @@ public class Repository implements Serializable {
 
     public static final File BRANCHES_DIR = join(GITLET_DIR, "Branches");
 
-    public static final File CURRENT_BRANCH = join(GITLET_DIR, "current branch");
+    public static final File CURRENT_BRANCH = join(GITLET_DIR, "current createBranch");
     /**
      * The head file represents the current commit.
      */
@@ -41,22 +41,22 @@ public class Repository implements Serializable {
      */
     public static final File index = join(GITLET_DIR, "index");
     /**
-     * points to the current commit.
-     */
-    String head;
-    /**
      * represents the added files in staging area before they are commited.
      */
     StagingArea stagingArea;
     /**
-     * Represents the current branch.
+     * Represents the current createBranch.
      */
     String curBranch;
-    
+    /**
+     * Tracks whether the Gitlet repository has been initialized.
+     */
+
     public boolean backupPerformed = false; // to backup GITLET_DIR state each time when the program terminates
 
     public void mapInitializations() {
         stagingArea = new StagingArea();
+        //blobs = new HashMap<String, Blob>();
     }
 
     public void validateInitialized() {
@@ -74,7 +74,6 @@ public class Repository implements Serializable {
 
         mapInitializations();
 
-        head = readContentsAsString(HEAD);
         curBranch = readContentsAsString(CURRENT_BRANCH);
         if (index.exists())// ensure that index file exists cuz it can not be existent if there aren`t files staged before.
             stagingArea = readObject(index, StagingArea.class);
@@ -102,16 +101,15 @@ public class Repository implements Serializable {
         writeObject(commitFile, initialCommit);
 
         // save the head state in HEAD file.
-        head = initialCommitId;
-        writeContents(HEAD, head);
+        writeContents(HEAD, initialCommitId);
 
-        // save the master branch in a file in BRANCHES_DIR.
+        // save the master createBranch in a file in BRANCHES_DIR.
         File masterBranchFile = new File(BRANCHES_DIR, "master");
-        writeContents(masterBranchFile, initialCommitId); // master file content is the most recent commit in the master branch which is the initial commit id
+        writeContents(masterBranchFile, initialCommitId); // master file content is the most recent commit in the master createBranch which is the initial commit id
 
-        // by default the first branch is the current branch is the master branch.
+        // by default the first createBranch is the current createBranch is the master createBranch.
         curBranch = "master";
-        writeContents(CURRENT_BRANCH ,  curBranch);// Save the current branch which is master in the CURRENT_BRANCH file.
+        writeContents(CURRENT_BRANCH, curBranch);// Save the current createBranch which is master in the CURRENT_BRANCH file.
     }
 
     public void add(String fileName) {
@@ -126,9 +124,9 @@ public class Repository implements Serializable {
             return;
         }
 
-            stagingArea.addForAddition(fileName);
-            // write the new staged files in index file
-            stagingArea.save();
+        stagingArea.addForAddition(fileName);
+        // write the new staged files in index file
+        stagingArea.save();
     }
 
     public void commit(String message) {
@@ -146,10 +144,11 @@ public class Repository implements Serializable {
         }
 
         // Get the parent commit (the current commit)
-        Commit parent = Commit.getCommit(head);
+        String headCommitId = readContentsAsString(HEAD);
+        Commit parent = Commit.getCommit(headCommitId);
         String parentId = parent.getId();
 
-        // Create new commit with message and parent
+        // Create new commit with message as its message and a parentId as its parent.
         Commit newCommit = new Commit(message, parentId);
         newCommit.setFiles(parent.getFiles());
 
@@ -168,34 +167,51 @@ public class Repository implements Serializable {
         // Save new commit
         newCommit.save();
 
+        // Get the id of the new Commit.
         String newCommitId = newCommit.getId();
 
-        // update the most recent commit in the curBranch to the new commit.
+        // public static final File CURRENT_BRANCH = join(GITLET_DIR, "current createBranch");
+
+        // set the new commit as the most recent commit in the current createBranch.
         File curBranchFile = new File(BRANCHES_DIR, curBranch);
         writeContents(curBranchFile, newCommitId);
 
         // Update HEAD
-        head = newCommitId;
-        writeContents(HEAD, head);
+        writeContents(HEAD, headCommitId);
 
         // Clear and save the staging area
         stagingArea.clear();
         stagingArea.save();
     }
 
-    public void rm(String fileMarkedForRemoval_Name) {
+    /**
+     * remove the file:
+     * If the file is staged, unstage it
+     * and if the file is tracked in the current commit, stage it for removal
+     * and remove the file from the working directory if the user has not already done so.
+     */
+    public void removeFile(String fileMarkedForRemoval_Name) {
+
+        Commit headCommit = Commit.getCommit(readContentsAsString(HEAD));
 
         validateInitialized();
+        boolean isTracked = headCommit.containsFile(fileMarkedForRemoval_Name);
+        //boolean stagedForRemoval = stagingArea.existentForRemoval(fileMarkedForRemoval_Name);
+        boolean stagedForAddition = stagingArea.existentForAddition(fileMarkedForRemoval_Name);
 
-        // Unstage the file if it is currently staged for addition.
-        if (stagingArea.existentForAddition(fileMarkedForRemoval_Name)) {
-            stagingArea.unStage(fileMarkedForRemoval_Name);
+
+        if (!stagedForAddition && !isTracked) {
+            System.out.println("No reason to remove the file.");
             return;
         }
 
-        // If the file is tracked in the current commit => stage it for removal from the CWD.
-        Commit headCommit = Commit.getCommit(head);
-        if (headCommit.containsFile(fileMarkedForRemoval_Name)) {
+        // Unstage the file if it is currently staged for addition.
+        if (stagedForAddition) {
+            stagingArea.unStage(fileMarkedForRemoval_Name);
+            stagingArea.save();
+        }
+        // If the file is tracked in the current commit => stage it for removal and delete it from the CWD.
+        if (isTracked) {
 
             // mark the file for removal.
             stagingArea.markForRemoval(fileMarkedForRemoval_Name);
@@ -203,24 +219,27 @@ public class Repository implements Serializable {
 
             // Delete the file from CWD.
             Utils.restrictedDelete(fileMarkedForRemoval_Name);
-        } else
-            System.out.println("No reason to remove the file.");
+        }
     }
 
-    public void find(String message) {
+    /**
+     * Prints out the ids of all commits that have the given commit message, one per line.
+     * If there are multiple such commits, it prints the ids out on separate lines.
+     */
+    public void findCommit(String commitMessage) {
 
         validateInitialized();
 
-        boolean foundCommits = false; // Indicating that there is at least one commit found with the given message.
+        boolean foundCommits = false; // Becomes true if at least one commit found with the given message.
 
         // retrieve the commit files.
-        List<String> commitFiles = Utils.plainFilenamesIn(COMMITS_DIR); // get all the commit file names from the COMMITS_DIR
-        for (String commitId : commitFiles) {
+        List<String> commitNames = Utils.plainFilenamesIn(COMMITS_DIR); // get all the commit file names from the COMMITS_DIR
+        for (String commitName : commitNames) {
 
             // get the commit object from the commit file.
-            Commit commit = Commit.getCommit(commitId);
-            if (commit.getMessage().equals(message)) {
-                System.out.println(commitId);
+            Commit commit = Commit.getCommit(commitName);
+            if (commit.getMessage().equals(commitMessage)) {
+                System.out.println(commitName);
 
                 foundCommits = true;
             }
@@ -234,7 +253,7 @@ public class Repository implements Serializable {
 
         validateInitialized();
 
-        Commit current = Commit.getCommit(head);// start iterate from the most recent commit.
+        Commit current = Commit.getCommit(readContentsAsString(HEAD));// start iterate from the most recent commit.
 
         while (current != null) {
 
@@ -274,13 +293,13 @@ public class Repository implements Serializable {
         */
     }
 
-    public void checkout(String fileName) {
+    public void checkoutFile(String fileName) {
 
         validateInitialized();
 
-        Commit curCommit = Commit.getCommit(head); // retrieve the commit(current commit) head points to.
+        Commit curCommit = Commit.getCommit(readContentsAsString(HEAD)); // Retrieve the current commit (head commit).
 
-        // Restore the file from the curCommit IF it contains it.
+        // Restore the file from the curCommit if it contains it.
         if (curCommit.containsFile(fileName)) {
 
             // get the file from the curCommit.
@@ -289,144 +308,144 @@ public class Repository implements Serializable {
             String fileContent = Blob.getBlob(fileId).getContent();// get the file content from it's blob.
 
             // Add the file in CWD if the file doesn't exist or overwrite the file if it exists.
-            File fileInCWD = new File(CWD, fileId);
+            File fileInCWD = new File(CWD, fileName);
             writeContents(fileInCWD, fileContent);
         } else
             System.out.println("File does not exist in that commit.");
     }
 
-    public void checkout(String commitId, String fileName) {
+    public void checkoutFile(String commitId, String fileName) {
 
         validateInitialized();
 
         Commit commit = Commit.getCommit(commitId); // retrieve the commit by its Id.
-        if (commit != null) {
 
-            // Restore the file from the commit IF it contains it.
-            if (commit.containsFile(fileName)) {
-
-                String fileId = commit.getFileId(fileName); // get the file from the curCommit.
-
-                String fileContent = Blob.getBlob(fileId).getContent();// get the file content from it's blob.
-
-                // Add the file in CWD if the file doesn't exist or overwrite the file if it exists.
-                File fileInCWD = new File(CWD, fileId);
-                writeContents(fileInCWD, fileContent);
-            } else // If no such a file in the commit has the given commitId.
-                System.out.println("File does not exist in that commit.");
-        } else // if no such a commit id.
+        if (commit == null) {
             System.out.println("No commit with that id exists.");
+            return;
+        }
+
+        if (!commit.containsFile(fileName)) {
+            System.out.println("File does not exist in that commit.");
+            return;
+        }
+
+        String fileId = commit.getFileId(fileName); // get the file from the curCommit.
+
+        String fileContent = Blob.getBlob(fileId).getContent();// get the file content from it's blob.
+
+        // Add the file in CWD if the file doesn't exist or overwrite the file if it exists.
+        File fileInCWD = new File(CWD, fileName);
+        writeContents(fileInCWD, fileContent);
     }
 
+    /**
+     * Checks out all files from the given createBranch into the working directory (CWD).
+     * Behavior:-
+     * 1. Validates the target createBranch exists and is not the current createBranch.
+     * 2. Aborts if untracked files in CWD would be overwritten.
+     * 3. For each file in the target createBranch:
+     *    - Overwrites existing CWD version (if present)
+     *    - Creates new files (if not present)
+     * 4. Deletes files tracked in current createBranch but absent in target createBranch.
+     */
     public void checkoutBranch(String branchName) {
 
         validateInitialized();
 
-        // Check if the branch already exists
-        File BranchFile  = new File(BRANCHES_DIR, branchName);
+        // Check if the createBranch already exists
+        File BranchFile = new File(BRANCHES_DIR, branchName);
         if (!BranchFile.exists()) {
-            System.out.println("No such branch exists.");
+            System.out.println("No such createBranch exists.");
             return;
         }
-        // get the branch (the most recent commit in that branch)
-        String branchId = readContentsAsString(BranchFile);
+        // get the createBranch head id(the id of the most recent commit in that createBranch)
+        String branchHeadId = readContentsAsString(BranchFile);
 
-        // if i am already in the branch branchName => then i go nowhere .
-        if (head.equals(branchId)) {
-            System.out.println("No need to checkout the current branch.");
+        // if that createBranch is the current createBranch, do nothing.
+        if (readContentsAsString(CURRENT_BRANCH).equals(branchName)) {
+            System.out.println("No need to checkoutFile the current createBranch.");
             return;
         }
 
-        // Retrieve all files in the commit at the head of the given branch.
-        Commit branchHead = Commit.getCommit(branchId);
+        Commit branchHeadCommit = Commit.getCommit(branchHeadId);
+        String headCommitID = readContentsAsString(HEAD);
 
-        // iterate all the files in CWD and if there is a file exists in the branchHead => update it in the CWD, if not => remove it.
-        List<String> cwdFiles = Utils.plainFilenamesIn(CWD); // retrieve all the files from the cwd.
-        Map<String, Integer> cwdFilesMap = new HashMap<String, Integer>();// create a map for constant time access.
-
-        assert cwdFiles != null;
-        for (String fileName : cwdFiles) { // iterate CWD files.
-
-            cwdFilesMap.put(fileName, 0);
+        // If there are files in the createBranch head commit need to be fetched but un tracked, Report it and exit.
+        for (String fileName : branchHeadCommit.getFiles().keySet()) {
 
             File fileInCWD = new File(CWD, fileName);
+            if (fileInCWD.exists() && !Commit.getCommit(headCommitID).containsFile(fileName) && !stagingArea.existentForAddition(fileName)) {
 
-            if (branchHead.containsFile(fileName)) { // check if file exists in the branch head commit to be updated to the branchHead version.
-
-                // find the id of the fileInBranchHead
-                String fileIdInBranchHead = branchHead.getFileId(fileName);
-
-                // find the id of the same file in CWD.
-                String fileIdInCWD = Utils.sha1(fileInCWD);
-
-                // if they are not the same then => overwrite the file in CWD
-                if (!fileIdInBranchHead.equals(fileIdInCWD)) {
-
-                    // check if the file is tracked.
-                    if (stagingArea.containsAddedFile(fileIdInCWD)) {
-                        String FileContentInBranchHead = Blob.getBlob(fileIdInBranchHead).getContent();
-                        writeContents(fileInCWD, FileContentInBranchHead);
-                    } else // the file is not tracked.
-                        System.out.println("There is an untracked file in the way; delete it, or add and commit it first.");
-                }
-            } else //delete the file from CWD.
-                restrictedDelete(fileInCWD);
-        }
-
-        // Iterate the file in Branch head to add the files are not there in CWD.
-        for (Map.Entry<String, String> entry : branchHead.getFiles().entrySet()) {
-
-            String fileName = entry.getKey();
-
-            // add the newFile if its not existent in CWD.
-            if (!cwdFilesMap.containsKey(fileName)) {
-                File newFile = new File(CWD, fileName);
-                String newFileContent = Blob.getBlob(branchHead.getFileId(fileName)).getContent();
-                writeContents(newFile, newFileContent);
+                System.out.println("There is an untracked file in the way; delete it, or add and commit it first.");
+                return;
             }
         }
 
-        // Update the current branch to the branchName.
-        curBranch = branchName;
+        // Add the createBranch head to CWD, overwriting the versions of the files that are already there if they exist
+        for (String fileName : branchHeadCommit.getFiles().keySet()) {
 
-        // update the file saves the current branch.
+            File fileInCWD = new File(CWD, fileName);
+
+            String FileContentInBranchHead = Blob.getBlob(branchHeadCommit.getFileId(fileName)).getContent();
+            writeContents(fileInCWD, FileContentInBranchHead);
+        }
+
+        // Delete any files tracked in the current commit but not in the checked out createBranch.
+        for (String fileName : Commit.getCommit(headCommitID).getFiles().keySet()) {
+
+            if(!branchHeadCommit.containsFile(fileName))
+                restrictedDelete(fileName);
+
+        }
+
+        // Update the current createBranch to the checked out createBranch.
+        curBranch = branchName;
         writeObject(CURRENT_BRANCH, curBranch);
 
         // Clear the staging area.
         stagingArea.clear();
     }
 
-    public void branch(String branchName) {
+    /**
+     * Create new branch with the given name and points it as the current head commit.
+     */
+    public void createBranch(String branchName) {
 
         validateInitialized();
 
-        // Check if the branch already exists
-        File branchFile  = new File(BRANCHES_DIR, branchName);
+        // Check if the createBranch already exists
+        File branchFile = new File(BRANCHES_DIR, branchName);
         if (branchFile.exists()) {
-            System.out.println("A branch with that name already exists.");
+            System.out.println("A createBranch with that name already exists.");
             return;
         }
 
-        // save the new branch in BRANCHES_DIR.
-        writeContents(branchFile, head);
+        // save the new createBranch in BRANCHES_DIR and its head commit is the current commit.
+        writeContents(branchFile, readContentsAsString(HEAD));
     }
 
-    public void rm_branch(String branchName) {
+    /**
+     *  Delete Branch with the given name.
+     *  The deleted createBranch can not be the curren branch.
+     *  Be sure that The deleted createBranch exists before deletion.
+     */
+    public void removeBranch(String branchName) {
 
         validateInitialized();
 
-        File branchFile  = new File(BRANCHES_DIR, branchName);
+        File branchFile = new File(BRANCHES_DIR, branchName);
         if (!branchFile.exists()) {
-            System.out.println("A branch with that name does not exist.");
+            System.out.println("A createBranch with that name does not exist.");
             return;
         }
 
-        if (curBranch.equals(branchName)) {
-            System.out.println("Cannot remove the current branch.");
+        if (readContentsAsString(CURRENT_BRANCH).equals(branchName)) {
+            System.out.println("Cannot remove the current createBranch.");
             return;
         }
 
-        // delete the branch file from the BRANCHES_DIR.
+        // delete the createBranch file from the BRANCHES_DIR.
         Utils.restrictedDelete(branchFile);
 
 
